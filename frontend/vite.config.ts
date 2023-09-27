b87b77a747d69d4b439ffa1edf73aed3815fc644
dallascrichmond <dallas.richmond@gov.bc.ca>
2023-09-27 16:19:13 -0700

    Added server objects in vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      include: ["**/*.tsx"],
+    })],
+  build: {
+    outDir: "build",
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 8080,
+    watch: {
+      usePolling: true,
+    },
+  },
+});

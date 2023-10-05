51c49af94650644444e0ee462d7cbc850d99663c
dallascrichmond <dallas.richmond@gov.bc.ca>
2023-10-05 14:20:36 -0700

    ESlint configured in frontend

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -2,18 +2,25 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "airbnb",
-    "airbnb/hooks",
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: "module",
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/react-in-jsx-scope': 0,
   },
 }

4492deb74fbdcd905f14039bbe48661ab5c97cbb
dallascrichmond <dallas.richmond@gov.bc.ca>
2023-10-05 15:56:11 -0700

    Basic Api with health endpoint set up

---
+++ api/routes/health-router.ts
@@ -0,0 +1,14 @@
+/**
+ * @summary This is the Health Endpoint for SET, its purpose is to provide health
+ *          checks to dockerization efforts to ensure the container is operating as expected
+ * @author LocalNewsTV, Dallascrichmond
+ */
+import express from 'express';
+import { getHealth } from '../controllers/health-controller';
+
+const router = express.Router();
+
+router.route('/health')
+  .get(getHealth);
+
+export default router;

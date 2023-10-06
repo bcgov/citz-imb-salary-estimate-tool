462772f0fd02a65a114ba38dab1878ac3f3f76f5
dallascrichmond <dallas.richmond@gov.bc.ca>
2023-10-05 18:06:09 -0700

    Added dist to gitignore

--- api/dist/middleware/globalErrorHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const globalErrorHandler = ({ res, next, }) => {
-    res.status(500).json({
-        message: 'Internal Server Error',
-    });
-    next();
-};
-exports.default = globalErrorHandler;

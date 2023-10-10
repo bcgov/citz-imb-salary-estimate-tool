b20fcff51a4735f469cbc22d55338c5307d93503
Brandon Bouchard <brandonjbouchard@gmail.com>
2023-10-10 13:08:29 -0700

    Fix casing for deploy.sh

--- .github/helpers/deploy.sh
+++ .github/helpers/deploy.sh
@@ -2,7 +2,7 @@
 
 # Use DeploymentConfigs to deploy the application to OpenShift.
 
-oc process -f /home/runner/work/citz-imb-salary-estimate-tool/citz-imb-salary-estimate-tool/OpenShift/templates/$DEPLOYMENT_CONFIG --namespace=$OPENSHIFT_DEV_NAMESPACE \
+oc process -f /home/runner/work/citz-imb-salary-estimate-tool/citz-imb-salary-estimate-tool/openshift/templates/$DEPLOYMENT_CONFIG --namespace=$OPENSHIFT_DEV_NAMESPACE \
   -p OPENSHIFT_NAMESPACE=$OPENSHIFT_DEV_NAMESPACE \
   -p APPLICATION_NAME=$APPLICATION_NAME \
   -p OPENSHIFT_SERVER_URL=$OPENSHIFT_SERVER_URL \

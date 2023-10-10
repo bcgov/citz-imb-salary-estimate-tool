d2d83cf06a87e741d4540d06e3c7a80788a99f23
Brandon Bouchard <brandonjbouchard@gmail.com>
2023-10-10 15:50:30 -0700

    Gut check for deploy values.

--- .github/helpers/deploy.sh
+++ .github/helpers/deploy.sh
@@ -3,7 +3,7 @@
 # Use DeploymentConfigs to deploy the application to OpenShift.
 
 oc process -f /home/runner/work/citz-imb-salary-estimate-tool/citz-imb-salary-estimate-tool/openshift/templates/$DEPLOYMENT_CONFIG --namespace="ec1236-tools" \
-  -p OPENSHIFT_NAMESPACE="ec1236-dev" \
-  -p APPLICATION_NAME="set-api" \
-  -p IMAGE_REPOSITORY="image-registry.apps.silver.devops.gov.bc.ca" | \
+  -p OPENSHIFT_NAMESPACE=${OPENSHIFT_NAMESPACE} \
+  -p APPLICATION_NAME=${APPLICATION_NAME} \
+  -p IMAGE_REPOSITORY=${IMAGE_REPOSITORY} | \
   oc apply -f -

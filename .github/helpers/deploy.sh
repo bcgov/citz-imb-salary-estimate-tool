86314f0e08735cc77e88e7e6ccbc2c7ae4fe82ea
Brandon Bouchard <brandonjbouchard@gmail.com>
2023-10-10 13:51:49 -0700

    Change namespace for template.

--- .github/helpers/deploy.sh
+++ .github/helpers/deploy.sh
@@ -2,7 +2,7 @@
 
 # Use DeploymentConfigs to deploy the application to OpenShift.
 
-oc process -f /home/runner/work/citz-imb-salary-estimate-tool/citz-imb-salary-estimate-tool/openshift/templates/$DEPLOYMENT_CONFIG --namespace=$OPENSHIFT_DEV_NAMESPACE \
+oc process -f /home/runner/work/citz-imb-salary-estimate-tool/citz-imb-salary-estimate-tool/openshift/templates/$DEPLOYMENT_CONFIG --namespace=$OPENSHIFT_TOOLS_NAMESPACE \
   -p OPENSHIFT_NAMESPACE=$OPENSHIFT_DEV_NAMESPACE \
   -p APPLICATION_NAME=$APPLICATION_NAME \
   -p IMAGE_REPOSITORY=$IMAGE_REPOSITORY | \

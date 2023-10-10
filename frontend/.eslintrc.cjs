faca719c4020ddfce4bf957f7d02ce671065c205
dallascrichmond <dallas.richmond@gov.bc.ca>
2023-10-10 10:44:44 -0700

    Prettier configured

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -9,19 +9,16 @@ module.exports = {
     'airbnb/hooks',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
+    'plugin:prettier/recommended',
   ],
-  overrides: [
-  ],
+  overrides: [],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: './frontend/tsconfig.json',
   },
-  plugins: [
-    '@typescript-eslint',
-    'react',
-  ],
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
   rules: {
     'react/react-in-jsx-scope': 0,
   },

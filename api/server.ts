f77055aedab60adc4aabc2ce7bf64cf95b530e6f
dallascrichmond <dallas.richmond@gov.bc.ca>
2023-10-11 15:03:23 -0700

    Hard coded port in server.ts

--- api/server.ts
+++ api/server.ts
@@ -3,17 +3,17 @@
  * @summary Server listener for SETS Express API
  * @author LocalNewsTV, Dallascrichmond
  */
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 import app from './express';
 
-dotenv.config();
+// dotenv.config();
 
-const port = process.env.API_PORT || 3000;
+// const port = process.env.API_PORT || 3000;
 
-app.listen(port, (err?:Error) => {
+app.listen(3000, (err?:Error) => {
   // eslint-disable-next-line no-console
   if(err){
     console.log("Error:", err);
   }
-  console.info(`[server]: Server started on port ${port}`);
+  console.info(`[server]: Server started on port 3000`);
 });

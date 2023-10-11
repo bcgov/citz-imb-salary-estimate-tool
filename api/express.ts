8a122083c6338b6281a46d30beb2b9ce8bd5f736
dallascrichmond <dallas.richmond@gov.bc.ca>
2023-10-11 14:04:09 -0700

    Striped express.ts to only essentials

--- api/express.ts
+++ api/express.ts
@@ -3,38 +3,38 @@
  * @author  LocalNewsTV, Dallascrichmond
  */
 import express, { Application } from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
-import swaggerUi from 'swagger-ui-express';
-import swaggerJSDoc from 'swagger-jsdoc';
-import rateLimit from 'express-rate-limit';
-import * as config from './config';
-import * as routers from './routes';
-import * as middleware from './middleware';
+// import morgan from 'morgan';
+// import cors from 'cors';
+// import swaggerUi from 'swagger-ui-express';
+// import swaggerJSDoc from 'swagger-jsdoc';
+// import rateLimit from 'express-rate-limit';
+// import * as config from './config';
+// import * as routers from './routes';
+// import * as middleware from './middleware';
 
 const app: Application = express();
 
 // Express middleware
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.static('public'));
-app.use(cors(config.cors));
-app.use(rateLimit(config.rateLimitConfig));
+// app.use(express.static('public'));
+// app.use(cors(config.cors));
+// app.use(rateLimit(config.rateLimitConfig));
 
-app.disable('x-powered-by');
+// app.disable('x-powered-by');
 // Routing information
-app.use(
-  '/api/api-docs',
-  swaggerUi.serve,
-  swaggerUi.setup(
-    swaggerJSDoc(config.swaggerConfig),
-  ),
-);
-app.use('/api', [
-  routers.healthRouter,
-]);
+// app.use(
+//   '/api/api-docs',
+//   swaggerUi.serve,
+//   swaggerUi.setup(
+//     swaggerJSDoc(config.swaggerConfig),
+//   ),
+// );
+// app.use('/api', [
+//   routers.healthRouter,
+// ]);
 
 // Integrate global error handler after routes to cover all ends.
-app.use(middleware.globalErrorHandler);
+// app.use(middleware.globalErrorHandler);
 
 export default app;

08ccf9c0d767332ab7b834e7051769f56c4c5990
Brandon Bouchard <brandonjbouchard@gmail.com>
2023-10-12 14:31:24 -0700

    Add '|' to deploy.sh command

--- .github/helpers/deploy.sh
+++ .github/helpers/deploy.sh
@@ -2,5 +2,5 @@
 
 # Use DeploymentConfigs to deploy the application to OpenShift.
 
-oc process -f /home/runner/work/citz-imb-salary-estimate-tool/citz-imb-salary-estimate-tool/openshift/templates/$DEPLOYMENT_CONFIG --namespace="ec1236-dev" \
+oc process -f /home/runner/work/citz-imb-salary-estimate-tool/citz-imb-salary-estimate-tool/openshift/templates/$DEPLOYMENT_CONFIG --namespace="ec1236-dev" | \
   oc apply -f -

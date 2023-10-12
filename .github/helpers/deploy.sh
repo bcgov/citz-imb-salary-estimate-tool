f728ef4b8fdbeb5b74913d20e54ffde65384601b
Brandon Bouchard <brandonjbouchard@gmail.com>
2023-10-12 14:20:02 -0700

    Remove unused deploy vars.

--- .github/helpers/deploy.sh
+++ .github/helpers/deploy.sh
@@ -3,7 +3,4 @@
 # Use DeploymentConfigs to deploy the application to OpenShift.
 
 oc process -f /home/runner/work/citz-imb-salary-estimate-tool/citz-imb-salary-estimate-tool/openshift/templates/$DEPLOYMENT_CONFIG --namespace="ec1236-dev" \
-  -p OPENSHIFT_NAMESPACE="ec1236-dev" \
-  -p APPLICATION_NAME=${APPLICATION_NAME} \
-  -p IMAGE_REPOSITORY=${IMAGE_REPOSITORY} | \
   oc apply -f -

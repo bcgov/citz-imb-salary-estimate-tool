d13513f915cb1abd41b3f9047e5f30ea7951e884
dallascrichmond <dallas.richmond@gov.bc.ca>
2023-10-18 14:23:55 -0700

    Inquiry controller set up

--- api/routes/inquiry-router.ts
+++ api/routes/inquiry-router.ts
@@ -1,32 +1,36 @@
 import express from 'express';
-import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '@prisma/client';
+import { getInquiry } from '../controllers/inquiry-controller';
 
 const router = express.Router();
-const prisma = new PrismaClient;
+// const prisma = new PrismaClient;
 
-router.get('/inquiries', async (req, res, next) => {
-    try {
-        const inquiries = await prisma.inquiry.findMany();
-        res.json(inquiries);
-    } catch (error) {
-        next(error);
-    }
-});
+// router.get('/inquiries', async (req, res, next) => {
+//     try {
+//         const inquiries = await prisma.inquiry.findMany();
+//         res.json(inquiries);
+//     } catch (error) {
+//         next(error);
+//     }
+// });
 
-router.get('/inquiries/:id', async (req, res) => {
-    res.status(200).send('Inquiries Endpoint working');
-});
+router.route('/inquiry')
+    .get(getInquiry);
 
-router.post('/inquiries', async (req, res) => {
-    res.status(200).send('Inquiries Endpoint working');
-});
+// router.get('/inquiries/:id', async (req, res) => {
+//     res.status(200).send('Inquiries Endpoint working');
+// });
 
-router.delete('/inquiries/:id', async (req, res) => {
-    res.status(200).send('Inquiries Endpoint working');
-});
+// router.post('/inquiries', async (req, res) => {
+//     res.status(200).send('Inquiries Endpoint working');
+// });
 
-router.patch('/inquiries/:id', async (req, res) => {
-    res.status(200).send('Inquiries Endpoint working');
-});
+// router.delete('/inquiries/:id', async (req, res) => {
+//     res.status(200).send('Inquiries Endpoint working');
+// });
+
+// router.patch('/inquiries/:id', async (req, res) => {
+//     res.status(200).send('Inquiries Endpoint working');
+// });
 
 export default router;

843e312b8d5f88d3733019a5edb09c914288819f
dallascrichmond <dallas.richmond@gov.bc.ca>
2023-10-19 11:41:28 -0700

    Updated dockerfile

--- api/startProd.sh
+++ api/startProd.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-pwd
+
 # Run migrations
-npx prisma db push --schema='dist/prisma/schema.prisma' --skip-generate
-# Seed data
+npx prisma db push --schema='prisma/schema.prisma' --skip-generate
+
+# Seed data (if needed)
 npx prisma db seed
-# Start the application
-npm run prod
+
+# Start the application in production mode
+npm start

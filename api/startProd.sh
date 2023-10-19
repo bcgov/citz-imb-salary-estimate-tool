f9cb53c5e7daf20b698417ef0212869473b404f8
dallascrichmond <dallas.richmond@gov.bc.ca>
2023-10-19 13:32:48 -0700

    Fixed .js issue with seed.js

--- api/startProd.sh
+++ api/startProd.sh
@@ -4,7 +4,7 @@ pwd
 npx prisma db push --schema='prisma/schema.prisma' --skip-generate
 
 # Seed data (if needed)
-npx prisma db seed
+# npx prisma db seed
 
 # Start the application in production mode
 npm prod

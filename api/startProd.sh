851d3b4478d4b584c18bea973c4a43182fea3a23
dallascrichmond <dallas.richmond@gov.bc.ca>
2023-10-19 16:41:01 -0700

    Commented out seed command for now

--- api/startProd.sh
+++ api/startProd.sh
@@ -3,6 +3,7 @@ pwd \
 # Run migrations
 npx prisma db push --schema='prisma/schema.prisma' --skip-generate \
 # Seed data
-npx prisma db seed \
+# TODO Make a check to see if data already exists
+# npx prisma db seed \
 # Start the application in production mode
 npm run start

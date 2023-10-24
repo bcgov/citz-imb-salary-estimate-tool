e8eccb8c1d2f657f0ee7029dc2a235e017dfe76a
Scott Toews <scott.toews@gov.bc.ca>
2023-10-24 14:18:36 -0700

    initialize react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './routes/root';
+import ErrorPage from './error-page';
 
 const queryClient = new QueryClient();
+const router = createBrowserRouter([
+  { path: '/', element: <Root />, errorElement: <ErrorPage /> }
+]);
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div>Salary Estimate Tool</div>
+      <RouterProvider router={router} />
     </QueryClientProvider>
   );
 }

0917c9970857fc01fad716e903c0e7a1d3285a9c
Scott Toews <scott.toews@gov.bc.ca>
2023-10-25 13:34:42 -0700

    updated linting rules

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -21,5 +21,7 @@ module.exports = {
   plugins: ['@typescript-eslint', 'react', 'prettier'],
   rules: {
     'react/react-in-jsx-scope': 0,
+    'react/function-component-definition': 0,
+    'react/jsx-props-no-spreading': 0,
   },
 };

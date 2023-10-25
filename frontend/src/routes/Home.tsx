d6db2d65272d9d74ea23f39335954f1460d8d913
Scott Toews <scott.toews@gov.bc.ca>
2023-10-25 12:27:24 -0700

    update to use keycloak

--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -1,17 +1,26 @@
 import { Button, Dialog } from '../components';
 import { Typography } from '@mui/material';
+import { useKeycloak } from '@bcgov/kc-react';
 
 export const Home = () => {
+  const { login, logout, state } = useKeycloak();
 
-  const handleClose = () => {
-    alert('Some day this will work');
-  };
+  const user = state?.userInfo;
 
-  const actions = <Button onClick={handleClose} color="primary">Login</Button>;
+  const actions = user ? (
+    <Button onClick={() => logout()}>Logout</Button>
+  ) : (
+    <Button onClick={() => login({ idpHint: 'idir' })}>Login</Button>
+  );
 
-
-  return <Dialog open={true} actions={actions} >
-    <Typography align='center' variant='h3'>Login Required</Typography>
-    <Typography align='center' variant='subtitle1'>To access this application, log in with your IDIR</Typography>
-  </Dialog>;
+  return (
+    <Dialog open={true} actions={actions}>
+      <Typography align="center" variant="h3">
+        Login Required
+      </Typography>
+      <Typography align="center" variant="subtitle1">
+        To access this application, log in with your IDIR
+      </Typography>
+    </Dialog>
+  );
 };

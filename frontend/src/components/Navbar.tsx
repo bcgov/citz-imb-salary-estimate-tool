008661c8cb2564752da3a836834fcba086c882ca
Scott Toews <scott.toews@gov.bc.ca>
2023-10-25 09:14:58 -0700

    created Navbar

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import { AppBar, Box, Toolbar } from '@mui/material';
+import Logo from '../assets/logo.png';
+
+type NavbarProps = {
+  children: React.ReactNode;
+};
+
+export const Navbar = (props: NavbarProps) => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{ 'background-color': '#003366' }}>
+        <Toolbar>
+          <Box
+            component="img"
+            sx={{ height: 100, mr: 2 }}
+            alt="Government of B.C."
+            src={Logo}
+          />
+          {props.children}
+        </Toolbar>
+        <Box sx={{ height: 5, 'background-color': '#FCBA19' }} />
+      </AppBar>
+    </Box>
+  );
+};
---
+++ frontend/src/styles.ts
@@ -0,0 +1,8 @@
+import { createTheme } from '@mui/material/styles';
+import '@bcgov/bc-sans/css/BC_Sans.css'
+
+export const BCTheme = createTheme({
+  typography: {
+    fontFamily: ['BC Sans', 'Noto Sans', 'Verdana', 'Arial', 'sans-serif'].join(','),
+  },
+});

56d60a60e4d279729ea036d8853e503b27c8311c
Scott Toews <scott.toews@gov.bc.ca>
2023-11-01 08:02:01 -0700

    create data factory hook

---
+++ frontend/src/hooks/useDataFactory/useData.Factory.ts
@@ -0,0 +1,29 @@
+import { QueryKey, useQuery } from '@tanstack/react-query';
+import { useMemo } from 'react';
+import { useAPI } from '../useAPI/useAPI';
+
+export const useDataFactory = (endPoint: string, dataId: string) => {
+  const queryKey: QueryKey = useMemo(
+    () => [endPoint, dataId],
+    [dataId, endPoint]
+  );
+
+  const { fetchData } = useAPI();
+
+  const query = useQuery({
+    queryKey,
+    queryFn: async () => {
+      let response;
+      if (dataId) {
+        response = await fetchData(`${endPoint}/${dataId}`);
+      } else {
+        response = await fetchData(endPoint);
+      }
+      return response;
+    },
+  });
+
+  return { ...query };
+};
+
+export default useDataFactory;

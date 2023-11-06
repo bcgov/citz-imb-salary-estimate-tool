c98b9d2556c0505fe88e685395fff7a0fa71830f
Scott Toews <scott.toews@gov.bc.ca>
2023-11-06 09:19:14 -0800

    created fetchAPI unit testing

---
+++ frontend/src/hooks/useAPI/fetchAPI.test.ts
@@ -0,0 +1,50 @@
+import { fetchAPI } from './fetchAPI';
+
+// Mock the global fetch function
+const mockFetch = jest.fn(() =>
+  Promise.resolve({
+    ok: true,
+    json: () => Promise.resolve('mock data'),
+  })
+);
+(global.fetch as jest.Mock) = mockFetch;
+
+describe('fetchAPI', () => {
+  beforeEach(() => {
+    (global.fetch as jest.Mock).mockClear();
+  });
+
+  it('returns data from the API for GET request', async () => {
+    const data = await fetchAPI('test', { method: 'GET' });
+    expect(data).toBe('mock data');
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    expect(global.fetch).toHaveBeenCalledWith('/api/test', { method: 'GET' });
+  });
+
+  it('returns null for POST request', async () => {
+    const data = await fetchAPI('test', { method: 'POST' });
+    expect(data).toBe('mock data');
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    expect(global.fetch).toHaveBeenCalledWith('/api/test', {
+      method: 'POST',
+    });
+  });
+
+  it('returns null for PATCH request', async () => {
+    const data = await fetchAPI('test', { method: 'PATCH' });
+    expect(data).toBe('mock data');
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    expect(global.fetch).toHaveBeenCalledWith('/api/test', {
+      method: 'PATCH',
+    });
+  });
+
+  it('returns null for DELETE request', async () => {
+    const data = await fetchAPI('test', { method: 'DELETE' });
+    expect(data).toBeNull();
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    expect(global.fetch).toHaveBeenCalledWith('/api/test', {
+      method: 'DELETE',
+    });
+  });
+});

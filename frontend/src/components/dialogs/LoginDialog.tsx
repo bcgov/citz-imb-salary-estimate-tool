3af6d9534fa8ce3ac6f2426b2b2edae9f78f2a7d
Scott Toews <scott.toews@gov.bc.ca>
2023-11-09 10:41:38 -0800

    moved login functionality to a specialized dialog

---
+++ frontend/src/components/dialogs/LoginDialog.tsx
@@ -0,0 +1,26 @@
+import { useKeycloak } from '@bcgov/kc-react';
+import { Typography } from '@mui/material';
+import { Button } from '../buttons/Button';
+import { Dialog } from './Dialog';
+
+export const LoginDialog = () => {
+  const { login } = useKeycloak();
+
+  return (
+    <Dialog
+      open
+      actions={
+        <Button onClick={() => login({ idpHint: 'idir' })}>Login</Button>
+      }
+    >
+      <Typography align="center" variant="h3">
+        Login
+      </Typography>
+      <Typography align="center" variant="subtitle1">
+        To access this application, log in with your IDIR
+      </Typography>
+    </Dialog>
+  );
+};
+
+export default LoginDialog;

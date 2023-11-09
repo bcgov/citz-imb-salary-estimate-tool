87416841935349a9deaf546399e6a4e4284decf1
Scott Toews <scott.toews@gov.bc.ca>
2023-11-09 11:06:10 -0800

    create home unit test

---
+++ frontend/src/pages/Home.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react';
+import { Home } from './Home';
+
+jest.mock('react-router-dom', () => ({
+  Navigate: () => <div>Navigate</div>,
+}));
+jest.mock('../components', () => ({
+  LoginDialog: () => <div>Login Dialog</div>,
+}));
+
+describe('Home ', () => {
+  it('should render', () => {
+    render(<Home />);
+    expect(screen.getByText('Login Dialog')).toBeInTheDocument();
+  });
+  it('should redirect when authenticated', () => {
+    render(<Home isAuthenticated />);
+    expect(screen.getByText('Navigate')).toBeInTheDocument();
+  });
+});

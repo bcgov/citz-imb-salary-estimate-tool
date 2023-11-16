154d1756473a916f760b4d3e4755af39824b3af7
Scott Toews <scott.toews@gov.bc.ca>
2023-11-16 12:52:36 -0800

    refactor authentication dialog unit test

--- frontend/src/components/dialogs/AuthenticationDialog.test.tsx
+++ frontend/src/components/dialogs/AuthenticationDialog.test.tsx
@@ -1,17 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import { AuthenticationDialog } from './AuthenticationDialog';
 
-jest.mock('@bcgov/citz-imb-kc-react', () => ({
-  useKeycloak: () => ({
-    login: jest.fn(),
-  }),
-}));
-
 jest.mock('../buttons/AuthenticationButton', () => ({
   AuthenticationButton: () => <div>test authentication button</div>,
 }));
 
-describe('LoginDialog', () => {
+describe('AuthenticationDialog', () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
@@ -22,17 +16,9 @@ describe('LoginDialog', () => {
   it('renders', () => {
     render(<AuthenticationDialog open title={title} onClose={onClose} />);
     expect(screen.getByText(title)).toBeInTheDocument();
-    expect(screen.getByText('test authentication button')).toBeInTheDocument();
     expect(
       screen.getByText('To access this application, log in with your IDIR')
     ).toBeInTheDocument();
-  });
-  it('calls login when login button is clicked', async () => {
-    render(<AuthenticationDialog open title={title} onClose={onClose} />);
-    // screen.debug();
-    // await act(() =>
-    //   userEvent.click(screen.getByRole('button', { name: 'Login' }))
-    // );
-    // expect(login).toHaveBeenCalled();
+    expect(screen.getByText('test authentication button')).toBeInTheDocument();
   });
 });

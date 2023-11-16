70bffd786a7a492f2a62631f8bd0cef81e37bfeb
Scott Toews <scott.toews@gov.bc.ca>
2023-11-16 10:40:59 -0800

    create usedata.factory unit test

---
+++ frontend/src/hooks/useDataFactory/useData.Factory.test.ts
@@ -0,0 +1,27 @@
+import { renderHook } from '@testing-library/react';
+import { useDataFactory } from './useData.Factory';
+
+jest.mock('@tanstack/react-query', () => ({
+  useQuery: jest.fn().mockReturnValue({ data: 'test data' }),
+}));
+
+describe('useDataFactory', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  const endPoint = 'test-endpoint';
+  const dataId = 'test-id';
+
+  it('renders correctly', () => {
+    const { result } = renderHook(useDataFactory, {
+      initialProps: { endPoint },
+    });
+    expect(result.current.data).toBe('test data');
+  });
+  it('renders correctly with dataId', () => {
+    const { result } = renderHook(useDataFactory, {
+      initialProps: { endPoint, dataId },
+    });
+    expect(result.current.data).toBe('test data');
+  });
+});

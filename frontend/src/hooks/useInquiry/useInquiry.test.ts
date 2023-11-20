f309127ecaf74ad0e734c84c80a9039a80f0b112
Scott Toews <scott.toews@gov.bc.ca>
2023-11-20 10:47:20 -0800

    updated useInquirey unit test

--- frontend/src/hooks/useInquiry/useInquiry.test.ts
+++ frontend/src/hooks/useInquiry/useInquiry.test.ts
@@ -2,19 +2,108 @@ import { renderHook } from '@testing-library/react';
 import { useInquiry } from './useInquiry';
 import { useDataFactory } from '../useDataFactory/useData.Factory';
 
-jest.mock('../useDataFactory/useData.Factory');
+jest.mock('../useDataFactory/useData.Factory', () => ({
+  useDataFactory: jest.fn().mockReturnValue({
+    data: [{ id: 1, name: 'test' }],
+    isLoading: false,
+    isError: false,
+  }),
+}));
+
+jest.mock('../../hooks', () => ({
+  useAPI: jest.fn().mockReturnValue({
+    fetchData: jest.fn(),
+  }),
+}));
 
 describe('useInquiry', () => {
-  it('calls useDataFactory with the correct parameters', () => {
-    const endPoint = 'inquiry';
-    const dataId = 'test-id';
-
-    jest.mock('../useDataFactory/useData.Factory');
-
-    renderHook(() => useInquiry(dataId));
-
-    expect(useDataFactory).toHaveBeenCalledWith({ endPoint, dataId });
+  beforeEach(() => {
+    jest.clearAllMocks();
   });
 
-  // Add more tests as needed
+  it('renders correctly without an id', () => {
+    (useDataFactory as jest.Mock).mockReturnValueOnce({
+      data: [{ id: 1, name: 'test' }],
+      isLoading: false,
+      isError: false,
+    });
+
+    jest.mock('../../hooks', () => ({
+      useAPI: jest.fn().mockReturnValue({
+        fetchData: jest.fn(),
+      }),
+    }));
+
+    const { result } = renderHook(() => useInquiry());
+
+    expect(useDataFactory).toHaveBeenCalledWith(
+      expect.objectContaining({
+        endPoint: 'inquiry',
+        dataId: '',
+      })
+    );
+    expect(result.current.data.length).toEqual(1);
+    expect(result.current.data[0]).toEqual({ id: 1, name: 'test' });
+  });
+
+  it('renders correctly with an id', () => {
+    (useDataFactory as jest.Mock).mockReturnValueOnce({
+      data: [{ id: 1, name: 'test' }],
+      isLoading: false,
+      isError: false,
+    });
+
+    jest.mock('../../hooks', () => ({
+      useAPI: jest.fn().mockReturnValue({
+        fetchData: jest.fn(),
+      }),
+    }));
+
+    const { result } = renderHook(() => useInquiry('test'));
+
+    expect(useDataFactory).toHaveBeenCalledWith(
+      expect.objectContaining({
+        endPoint: 'inquiry',
+        dataId: 'test',
+      })
+    );
+    expect(result.current.data.length).toEqual(1);
+    expect(result.current.data[0]).toEqual({ id: 1, name: 'test' });
+  });
+
+  it('renders correctly when loading', () => {
+    (useDataFactory as jest.Mock).mockReturnValueOnce({
+      data: [{ id: 1, name: 'test' }],
+      isLoading: true,
+      isError: false,
+    });
+
+    jest.mock('../../hooks', () => ({
+      useAPI: jest.fn().mockReturnValue({
+        fetchData: jest.fn(),
+      }),
+    }));
+
+    const { result } = renderHook(() => useInquiry());
+
+    expect(result.current.data.length).toEqual(0);
+  });
+
+  it('renders correctly when error', () => {
+    (useDataFactory as jest.Mock).mockReturnValueOnce({
+      data: [{ id: 1, name: 'test' }],
+      isLoading: false,
+      isError: true,
+    });
+
+    jest.mock('../../hooks', () => ({
+      useAPI: jest.fn().mockReturnValue({
+        fetchData: jest.fn(),
+      }),
+    }));
+
+    const { result } = renderHook(() => useInquiry());
+
+    expect(result.current.data.length).toEqual(0);
+  });
 });

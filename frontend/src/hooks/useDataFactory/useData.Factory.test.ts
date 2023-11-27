4fceb75172eea0d9ceff0477456c1191ce965290
Scott Toews <scott.toews@gov.bc.ca>
2023-11-27 09:41:50 -0800

    update useDataFactory tests

--- frontend/src/hooks/useDataFactory/useData.Factory.test.ts
+++ frontend/src/hooks/useDataFactory/useData.Factory.test.ts
@@ -1,8 +1,15 @@
-import { renderHook } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react';
 import { useDataFactory } from './useData.Factory';
+import { useAPI } from '../useAPI/useAPI';
 
 jest.mock('@tanstack/react-query', () => ({
-  useQuery: jest.fn().mockReturnValue({ data: 'test data' }),
+  useQuery: jest.fn().mockReturnValue({ data: ['test data'] }),
+}));
+jest.mock('../useAPI/useAPI', () => ({
+  useAPI: jest.fn().mockReturnValue({
+    appendItem: jest.fn().mockReturnValue({ data: 'test data' }),
+    fetchData: jest.fn().mockReturnValue({ data: ['test data'] }),
+  }),
 }));
 
 describe('useDataFactory', () => {
@@ -12,17 +19,36 @@ describe('useDataFactory', () => {
   const endPoint = 'test-endpoint';
   const dataId = 'test-id';
 
-  it('renders correctly', () => {
-    const { result } = renderHook(useDataFactory, {
+  it('returns data correctly', async () => {
+    const { result } = await renderHook(useDataFactory, {
       initialProps: { endPoint },
     });
-    expect(result.current.data).toBe('test data');
+
+    expect(result.current.data[0]).toBe('test data');
   });
 
-  it('renders correctly with dataId', () => {
+  it('returns data correctly with dataId', () => {
     const { result } = renderHook(useDataFactory, {
       initialProps: { endPoint, dataId },
     });
-    expect(result.current.data).toBe('test data');
+    expect(result.current.data[0]).toBe('test data');
+  });
+
+  it('returns append correctly', () => {
+    (useAPI as jest.Mock).mockReturnValue({
+      appendItem: jest.fn().mockReturnValue({ data: 'test data' }),
+    });
+
+    const { result } = renderHook(useDataFactory, {
+      initialProps: { endPoint },
+    });
+
+    act(() => {
+      result.current.append('test data');
+    });
+
+    expect(result.current.append).toBeDefined();
+    expect(useAPI().appendItem).toHaveBeenCalledWith(endPoint, 'test data');
+    expect(result.current.append('test data')).toEqual({ data: 'test data' });
   });
 });

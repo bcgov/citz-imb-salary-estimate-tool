f19c208cc80d654192b8c8ce39197d7aac969e0c
dallascrichmond <dallas.richmond@gov.bc.ca>
2023-11-30 15:00:58 -0800

    Home test updates

--- frontend/src/pages/Home.test.tsx
+++ frontend/src/pages/Home.test.tsx
@@ -1,53 +1,71 @@
-import { render } from '@testing-library/react';
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable global-require */
+import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import { Home } from './Home'; // Update the path accordingly
-import { useAuthentication } from '../hooks';
+import { Home } from './Home';
 
+jest.mock('react-router-dom', () => ({
+  Navigate: () => <div>Navigate</div>,
+}));
+jest.mock('../components', () => ({
+  AuthenticationDialog: () => <div>Authentication Required</div>,
+  Dialog: () => (
+    <div>
+      You have not been assigned one of the necessary roles to access this
+      application.
+    </div>
+  ),
+}));
 jest.mock('../hooks', () => ({
-  useAuthentication: jest.fn(() => ({
-    isAuthenticated: false,
-    hasRole: jest.fn(),
-  })),
+  useAuthentication: jest
+    .fn()
+    .mockReturnValue({ isAuthenticated: false, hasRole: jest.fn() }),
 }));
 
 describe('Home Component', () => {
-  it('renders AuthenticationDialog when not authenticated', () => {
-    const { getByText } = render(<Home />, { wrapper: MemoryRouter });
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-    expect(getByText('Authentication Required')).toBeInTheDocument();
+  it('renders AuthenticationDialog when not authenticated', () => {
+    render(<Home />, { wrapper: MemoryRouter });
+    expect(screen.getByText('Authentication Required')).toBeInTheDocument();
   });
 
   it('renders Dialog when authenticated but has no role', () => {
-    const { getByText } = render(<Home />, { wrapper: MemoryRouter });
-    (useAuthentication as jest.Mock).mockReturnValueOnce({
+    (require('../hooks').useAuthentication as jest.Mock).mockReturnValueOnce({
       isAuthenticated: true,
       hasRole: jest.fn(),
     });
 
+    render(<Home />, { wrapper: MemoryRouter });
+
     expect(
-      getByText(
+      screen.getByText(
         'You have not been assigned one of the necessary roles to access this application.'
       )
     ).toBeInTheDocument();
   });
 
   it('renders Navigate to "/Admin" when authenticated with "admin" role', () => {
-    const { container } = render(<Home />, { wrapper: MemoryRouter });
-    (useAuthentication as jest.Mock).mockReturnValueOnce({
+    (require('../hooks').useAuthentication as jest.Mock).mockReturnValueOnce({
       isAuthenticated: true,
       hasRole: jest.fn((role) => role === 'admin'),
     });
 
-    expect(container.querySelector('a[href="/Admin"]')).toBeTruthy();
+    render(<Home />, { wrapper: MemoryRouter });
+
+    expect(screen.getByText('Navigate')).toBeInTheDocument();
   });
 
   it('renders Navigate to "/Inquiry" when authenticated with "hm", "shr", or "adm" role', () => {
-    const { container } = render(<Home />, { wrapper: MemoryRouter });
-    (useAuthentication as jest.Mock).mockReturnValueOnce({
+    (require('../hooks').useAuthentication as jest.Mock).mockReturnValueOnce({
       isAuthenticated: true,
       hasRole: jest.fn((role) => ['hm', 'shr', 'adm'].includes(role)),
     });
 
-    expect(container.querySelector('a[href="/Inquiry"]')).toBeTruthy();
+    render(<Home />, { wrapper: MemoryRouter });
+
+    expect(screen.getByText('Navigate')).toBeInTheDocument();
   });
 });

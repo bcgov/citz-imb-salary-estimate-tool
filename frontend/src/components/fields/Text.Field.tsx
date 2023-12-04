32c6f9529349995d04fa5f686277575aa0135587
Scott Toews <scott.toews@gov.bc.ca>
2023-12-04 14:20:38 -0800

    flesh out text field

--- frontend/src/components/fields/Text.Field.tsx
+++ frontend/src/components/fields/Text.Field.tsx
@@ -1,8 +1,14 @@
+import { OutlinedInput, InputLabel, FormControl } from '@mui/material';
 import { FieldProps } from './FieldProps.d';
 
 export const TextField = (props: FieldProps) => {
-  const { value } = props;
-  return <div>TODO: TextField: {value as string}</div>;
+  const { label, value } = props;
+  return (
+    <FormControl sx={{ m: 1 }}>
+      <InputLabel htmlFor={label}>{label}</InputLabel>
+      <OutlinedInput id={label} value={value} label={label} />
+    </FormControl>
+  );
 };
 
 export default TextField;

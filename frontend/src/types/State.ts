df45b9ed6741dc470e4913d8e7e2a4fd6dc84daf
Scott Toews <scott.toews@gov.bc.ca>
2023-12-04 11:24:08 -0800

    updated description

--- frontend/src/types/State.ts
+++ frontend/src/types/State.ts
@@ -1,7 +1,12 @@
 /**
  * State enum
  * @enum {number}
- * any changes to this enum must also be made in the StatusCell.tsx file
+ * new = 1,
+ * draft = 2,
+ * submitted = 3,
+ * reviewed = 4,
+ * approved = 5,
+ * rejected = 6,
  */
 export enum State {
   new = 1,

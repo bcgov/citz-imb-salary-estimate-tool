aef78ab066d61cacfd903e9c7ae38c9daa93bacb
Scott Toews <scott.toews@gov.bc.ca>
2023-12-04 13:54:31 -0800

    apply date localization to the app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,38 @@
 import { Box, Paper } from '@mui/material';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterLuxon } from '@mui/x-date-pickers/AdapterLuxon';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Route, Routes } from 'react-router-dom';
 import { Footer, Navbar } from './components';
-import { Home, Inquiry, InquiryNew } from './pages';
 import { useAuthentication } from './hooks';
+import { Home, Inquiry, InquiryNew } from './pages';
 
 const App = () => {
   const { KeycloakProvider } = useAuthentication();
 
   return (
     <KeycloakProvider>
-      <Paper
-        elevation={0}
-        sx={{
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-        }}
-      >
-        <Box sx={{ flexGrow: 1 }}>
-          <Navbar title="Salary Estimation Tool" />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/Inquiry" element={<Inquiry />} />
-            <Route path="/Inquiry/New" element={<InquiryNew />} />
-          </Routes>
-        </Box>
-        <Footer />
-      </Paper>
-      <ReactQueryDevtools initialIsOpen={false} />
+      <LocalizationProvider dateAdapter={AdapterLuxon}>
+        <Paper
+          elevation={0}
+          sx={{
+            height: '100vh',
+            display: 'flex',
+            flexDirection: 'column',
+          }}
+        >
+          <Box sx={{ flexGrow: 1 }}>
+            <Navbar title="Salary Estimation Tool" />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/Inquiry" element={<Inquiry />} />
+              <Route path="/Inquiry/New" element={<InquiryNew />} />
+            </Routes>
+          </Box>
+          <Footer />
+        </Paper>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </LocalizationProvider>
     </KeycloakProvider>
   );
 };

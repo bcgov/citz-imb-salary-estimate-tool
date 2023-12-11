2b8f607ec5f728e95d6e1cd85342a8ab340a822e
dallascrichmond <dallas.richmond@gov.bc.ca>
2023-12-11 10:48:08 -0800

    Tabs work, but need to be separated out into own components

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,21 +1,17 @@
-import { Box } from '@mui/material';
-// import { Navigate } from 'react-router-dom';
-import Typography from '@mui/material/Typography';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import { useState } from 'react';
-// import { ErrorDialog, Loading, TableContainer } from '../components';
-// import { useAuthentication, useInquiry, useUser } from '../hooks';
-// import { InquiryData } from '../types';
+import React, { useState } from 'react';
+import { Box, Typography, Tabs, Tab } from '@mui/material';
+import { Navigate } from 'react-router-dom';
+import { ErrorDialog, Loading, TableContainer } from '../components';
+import { useAuthentication, useInquiry, useUser } from '../hooks';
+import { InquiryData } from '../types';
 
-interface TabPanelProps {
-  // eslint-disable-next-line react/require-default-props
+interface TabPanelProps extends React.HTMLAttributes<HTMLDivElement> {
   children?: React.ReactNode;
   index: number;
   value: number;
 }
 
-function CustomTabPanel(props: TabPanelProps) {
+const CustomTabPanel = (props: TabPanelProps) => {
   const { children, value, index, ...other } = props;
 
   return (
@@ -33,116 +29,108 @@ function CustomTabPanel(props: TabPanelProps) {
       )}
     </div>
   );
-}
+};
 
-function a11yProps(index: number) {
+CustomTabPanel.defaultProps = {
+  children: '',
+};
+
+const a11yProps = (index: number) => {
   return {
     id: `simple-tab-${index}`,
     'aria-controls': `simple-tabpanel-${index}`,
   };
-}
+};
 
 const Home = () => {
   const [value, setValue] = useState(0);
+  const { isAuthenticated, hasRole } = useAuthentication();
+
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
 
-  // const { isAuthenticated, hasRole } = useAuthentication();
-  // const {
-  //   data: inquiryData,
-  //   columns: inquiryColumns,
-  //   isLoading: inquiriesLoading,
-  //   isError: inquiriesError,
-  //   error: inquiryError,
-  //   append,
-  // } = useInquiry();
+  const {
+    data: inquiryData,
+    columns: inquiryColumns,
+    isLoading: inquiriesLoading,
+    isError: inquiriesError,
+    error: inquiryError,
+    append,
+  } = useInquiry();
 
-  // const {
-  //   data: userData,
-  //   columns: userColumns,
-  //   isLoading: usersAreLoading,
-  //   isError: usersError,
-  //   error: userError,
-  // } = useUser();
+  const {
+    data: userData,
+    columns: userColumns,
+    isLoading: usersAreLoading,
+    isError: usersError,
+    error: userError,
+  } = useUser();
 
-  // const addItem = async () => {
-  //   const newItem: InquiryData = {
-  //     status_id: 1,
-  //     inquiry_submission_date: new Date(),
-  //     inquiry_completion_date: new Date('2023-11-01T20:02:34.516Z'),
-  //     candidate_first_name: 'Mick',
-  //     candidate_last_name: 'Swagger',
-  //     current_position_number: 293847,
-  //     current_position_title: 'Full Stack Developer',
-  //     current_ministry_id: 'CITZ',
-  //     current_annual_salary: 80000,
-  //     current_mccf_classification_id: 3,
-  //     experience_level_id: 2,
-  //     new_position_number: 473829,
-  //     new_position_title: 'Scrum Master',
-  //     new_mccf_classification_id: 4,
-  //     appointment_type_id: 1,
-  //     process_type_id: 2,
-  //     salary_estimate: 100000,
-  //     hm_comment: 'Here is the info for the successful candidate',
-  //     shr_comment: 'Here is the salary estimate',
-  //     adm_comment: 'Everything has been approved',
-  //   };
-  //   append(newItem);
-  // };
+  const addItem = async () => {
+    const newItem: InquiryData = {
+      status_id: 1,
+      inquiry_submission_date: new Date(),
+      inquiry_completion_date: new Date('2023-11-01T20:02:34.516Z'),
+      candidate_first_name: 'Mick',
+      candidate_last_name: 'Swagger',
+      current_position_number: 293847,
+      current_position_title: 'Full Stack Developer',
+      current_ministry_id: 'CITZ',
+      current_annual_salary: 80000,
+      current_mccf_classification_id: 3,
+      experience_level_id: 2,
+      new_position_number: 473829,
+      new_position_title: 'Scrum Master',
+      new_mccf_classification_id: 4,
+      appointment_type_id: 1,
+      process_type_id: 2,
+      salary_estimate: 100000,
+      hm_comment: 'Here is the info for the successful candidate',
+      shr_comment: 'Here is the salary estimate',
+      adm_comment: 'Everything has been approved',
+    };
+    append(newItem);
+  };
 
-  // if (!isAuthenticated) return <Navigate replace to="/login" />;
+  if (!isAuthenticated) return <Navigate replace to="/login" />;
 
-  // if (usersAreLoading || inquiriesLoading) return <Loading />;
+  if (usersAreLoading || inquiriesLoading) return <Loading />;
 
-  // if (usersError) return <ErrorDialog error={userError} />;
+  if (usersError) return <ErrorDialog error={userError} />;
 
-  // if (inquiriesError) return <ErrorDialog error={inquiryError} />;
+  if (inquiriesError) return <ErrorDialog error={inquiryError} />;
 
   return (
     <Box sx={{ width: '100%' }}>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          aria-label="basic tabs example"
-        >
-          <Tab label="Item One" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
+        <Tabs value={value} onChange={handleChange} aria-label="Table Tabs">
+          <Tab label="Inquiries" {...a11yProps(0)} />
+          <Tab label="Users" {...a11yProps(1)} />
         </Tabs>
       </Box>
       <CustomTabPanel value={value} index={0}>
-        Item One
+        <TableContainer
+          rows={inquiryData}
+          columns={inquiryColumns}
+          tableName="Inquiries"
+          addAction={addItem}
+        />
       </CustomTabPanel>
       <CustomTabPanel value={value} index={1}>
-        Item Two
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={2}>
-        Item Three
+        {hasRole('admin') ? (
+          <TableContainer
+            rows={userData}
+            columns={userColumns}
+            tableName="Users"
+            getRowId={(row) => row.guid}
+          />
+        ) : (
+          <div />
+        )}
       </CustomTabPanel>
     </Box>
   );
 };
 
 export default Home;
-
-/* <Box p={2}>
-<TableContainer
-  rows={inquiryData}
-  columns={inquiryColumns}
-  tableName="Inquiries"
-  addAction={addItem}
-/>
-{hasRole('admin') ? (
-  <TableContainer
-    rows={userData}
-    columns={userColumns}
-    tableName="Users"
-    getRowId={(row) => row.guid}
-  />
-) : (
-  <div />
-)}
-</Box> */

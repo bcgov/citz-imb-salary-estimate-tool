1c001dfa0d01343210dfde04f37c42cc77b8fe60
dallascrichmond <dallas.richmond@gov.bc.ca>
2023-12-21 14:51:48 -0800

    Added csv to string function

--- frontend/src/utils/UploadSalaryData.tsx
+++ frontend/src/utils/UploadSalaryData.tsx
@@ -22,3 +22,38 @@ export interface ISalaryData {
   ama?: number;
   created_at?: Date;
 }
+
+/**
+ * @description Takes a CSV file and converts it into a string for processing.
+ * @param {File} file The incoming CSV file.
+ * @returns {Promise<string>} A promise of the converted string.
+ * @author dbarkowsky
+ */
+export const csvFileToString = (file: File): Promise<string> => {
+  return new Promise<string>((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+      if (event.target && event.target.result) {
+        const csvData = (event.target.result as string)
+          .replace(/\r\n/g, '\n')
+          .replace(/^,+$/gm, '');
+        resolve(csvData);
+      } else {
+        reject(new Error('Failed to read file.'));
+      }
+    };
+
+    reader.onerror = (event) => {
+      reject(
+        new Error(
+          `Error reading file: ${
+            event.target?.error?.message || 'Unknown error'
+          }`
+        )
+      );
+    };
+
+    reader.readAsText(file);
+  });
+};

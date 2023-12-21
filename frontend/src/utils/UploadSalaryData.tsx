bc5056fd13adcb9304e471e47ff9a481a2236e71
dallascrichmond <dallas.richmond@gov.bc.ca>
2023-12-21 14:47:11 -0800

    Added papaparse as a dependency

---
+++ frontend/src/utils/UploadSalaryData.tsx
@@ -0,0 +1,24 @@
+import papa from 'papaparse';
+
+export interface ISalaryData {
+  id?: number;
+  organization?: string;
+  program?: string;
+  program_division?: string;
+  position_number?: number;
+  title?: string;
+  job_code?: number;
+  classification?: string;
+  appointment?: string;
+  schedule?: string;
+  supervisor_position_number?: number;
+  employee_id?: number;
+  employee_job_code?: number;
+  employee_classification?: string;
+  step?: number;
+  position_job_code_max_annual_rate?: number;
+  employee_job_code_max_annual_rate?: number;
+  abbr?: number;
+  ama?: number;
+  created_at?: Date;
+}

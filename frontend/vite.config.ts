bd0e4fbdda2f1fa74c791ac3199242d9302fbd6b
Scott Toews <scott.toews@gov.bc.ca>
2024-01-03 14:12:11 -0800

    updated vite config to use alias imports

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'build',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
   server: {
     host: '0.0.0.0',

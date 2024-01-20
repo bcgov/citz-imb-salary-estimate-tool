2c0c0ddead8426dc29c290cbc7683484cab840c0
Scott Toews <scott.toews@gov.bc.ca>
2024-01-20 12:05:57 -0800

    refactor imports

--- backend/src/modules/ministry/router.ts
+++ backend/src/modules/ministry/router.ts
@@ -1,9 +1,7 @@
 import express from 'express';
 import type { EntitySchema } from 'typeorm';
 import dataSource from '../../dataSource';
-import { createController } from '../common/controller/controller.factory';
-import { createRepository } from '../common/repository/repository.factory';
-import { createService } from '../common/service/service.factory';
+import { createController, createRepository, createService } from '../common';
 import { Ministry } from './entity';
 
 const dataRepository = createRepository<Ministry>(

116eea0b29324558b34815c4cbef55a2213ec491
BradyMitch <braden.jr.mitch@gmail.com>
2024-01-23 09:58:58 -0800

    Changed keycloak after login, logout

--- backend/keycloak/keycloakConfig.ts
+++ backend/keycloak/keycloakConfig.ts
@@ -6,13 +6,11 @@ const { DEBUG } = process.env;
 // Keycloak auth integration configuration.
 export const KEYCLOAK_OPTIONS = {
   afterUserLogin: (user: KeycloakUser) => {
-    if (DEBUG)
-      console.log(`DEBUG: afterUserLogin in config KEYCLOAK_OPTIONS called. ${user.display_name}`);
+    if (DEBUG) console.log(`DEBUG: ${user.display_name} has logged in.`);
     activateUser(user);
   },
   afterUserLogout: (user: KeycloakUser) => {
-    if (DEBUG)
-      console.log(`DEBUG: afterUserLogout in config KEYCLOAK_OPTIONS called. ${user.display_name}`);
+    if (DEBUG) console.log(`DEBUG: ${user.display_name} has logged out.`);
     console.log(`${user?.display_name ?? 'Unknown'} has logged out.`);
   },
 } as KCOptions;

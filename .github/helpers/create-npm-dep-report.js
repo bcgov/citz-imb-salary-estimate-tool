41d051de0d3f2b69292232403ef6caea0e1f4b4d
Noor Chasib <noorchasib@gmail.com>
2024-01-23 15:17:53 -0800

    add e2e to npm dependency check workflows

--- .github/helpers/create-npm-dep-report.js
+++ .github/helpers/create-npm-dep-report.js
@@ -5,7 +5,7 @@ const outdatedDeps = require(path.resolve(
 ));
 
 const LOCAL_TEST = false;
-const TEST_PACKAGEJSON_PATHS = ["src/frontend", "src/backend"];
+const TEST_PACKAGEJSON_PATHS = ["src/frontend", "src/backend", "src/e2e"];
 
 /**
  * THIS FILE DOES NOT REQUIRE ANY EDITING.

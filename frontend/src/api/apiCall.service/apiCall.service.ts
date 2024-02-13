6e225979b75f65f3b28c3c418c0755b3e18904d1
Scott Toews <scott.toews@gov.bc.ca>
2024-02-13 13:09:37 -0800

    changed to no content to return empty array

--- frontend/src/api/apiCall.service/apiCall.service.ts
+++ frontend/src/api/apiCall.service/apiCall.service.ts
@@ -1,7 +1,11 @@
+import { httpStatusCode } from '@/utils';
+
 export const apiCall = async (url: string, payload: RequestInit) => {
   const response = await fetch(url, payload);
 
   if (response.ok) {
+    if (response.status === httpStatusCode.NO_CONTENT) return [];
+
     const data = await response.json();
 
     return data;
